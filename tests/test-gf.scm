26b6414d073f4c3a153966640534e27251e21c55
jkmsk <jkmsk@spinode.fr>
2026-07-13 17:11:13 +0200

    card and order implemented in make-gf (galois field)

--- tests/test-gf.scm
+++ tests/test-gf.scm
@@ -15,6 +15,27 @@
 (test-equal "deg: GF(256) X^8+X^4+X^3+X^2+1 (100011101) has degree 8"
   8 ((make-gf #x11D) 'deg))
 
+(test-equal "card: GF(2)   has 2 elements"
+  2 ((make-gf #x3) 'card))
+
+(test-equal "card: GF(16)  has 16 elements"
+  16 ((make-gf #x13) 'card))
+
+(test-equal "card: GF(256) has 256 elements"
+  256 ((make-gf #x11D) 'card))
+
+(test-equal "order: GF(2)   Fq* has order 1"
+  1 ((make-gf #x3) 'order))
+
+(test-equal "order: GF(16)  Fq* has order 15"
+  15 ((make-gf #x13) 'order))
+
+(test-equal "order: GF(256) Fq* has order 255"
+  255 ((make-gf #x11D) 'order))
+
+(test-equal "order = card - 1"
+  (- ((make-gf #x11D) 'card) 1) ((make-gf #x11D) 'order))
+
 (test-error "an unknown message raises an error"
   #t ((make-gf #x11D) 'unknown-op))
 
